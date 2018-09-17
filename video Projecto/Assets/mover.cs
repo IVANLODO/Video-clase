e1e4d2402dd36b3d3d626dee9bd3575caaa078fd
German3dVicens <43268784+German3dVicens@users.noreply.github.com>
2018-09-17 21:06:49 +0200

    Estos son mis cambios para la prueba amigos

---
+++ video Projecto/Assets/mover.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rigidBodyMovement : MonoBehaviour
+{
+
+    public bool jump = false;
+    public float moveForce = 20f;
+    public float maxSpeed = 3f;
+    public float jumpForce = 200f;
+
+    private Rigidbody2D rBody;
+
+
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody2D>();
+        rBody.freezeRotation = true;
+    }
+
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+
+        if (h * rBody.velocity.x < maxSpeed)
+            rBody.AddForce(Vector2.right * h * moveForce);
+
+        if (Mathf.Abs(rBody.velocity.x) > maxSpeed)
+            rBody.velocity = new Vector2(Mathf.Sign(rBody.velocity.x) * maxSpeed, rBody.velocity.y);
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rBody.AddForce(new Vector2(0f, jumpForce));
+        }
+    }
+
+}
